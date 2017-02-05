bf968b69102acb1e7f2317f1358a35ee4447b87a
WillyReyno <reyno.willy@gmail.com>
2017-02-05 23:23:52 +0100

    Exercice 7.2

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -5,9 +5,14 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Un article</div>
+                    <div class="panel-heading">{{ $article->title }}</div>
                     <div class="panel-body">
-                        {{-- Afficher un article --}}
+                        {{ $article->content }}
+
+                        <br>
+                        <br>
+
+                        <strong>{{ $article->user->name }}</strong>
                     </div>
                 </div>
             </div>
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -9,11 +9,23 @@
 
                     <div class="panel-body">
                         @if(Auth::check())
+                            <h3>Vos informations</h3>
                             <ul>
                                 <li>{{ Auth::user()->name }}</li>
                                 <li>{{ Auth::user()->email }}</li>
                                 <li>{{ Auth::user()->created_at }}</li>
                             </ul>
+
+                            <h3>Vos articles</h3>
+                            <ul>
+                                @forelse(Auth::user()->articles as $article)
+                                    <li><a href="{{ route('article.show', $article->id) }}">{{ $article->title }}</a></li>
+                                @empty
+                                    Vous n'avez pas encore publié d'article.
+                                @endforelse
+                            </ul>
+                        @else
+                            Vous n'êtes pas connecté.
                         @endif
                     </div>
                 </div>
